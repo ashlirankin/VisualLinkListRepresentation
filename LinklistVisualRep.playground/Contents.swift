7f3bb768cd6df123d54aa47f31fc5332bf7c9a2f
Ashlirankin18 <ashlirankin18@gmail.com>
2019-05-16 17:11:21 -0400

    reverted vc changes

--- LinklistVisualRep.playground/Contents.swift
+++ LinklistVisualRep.playground/Contents.swift
@@ -1,5 +1,65 @@
 import UIKit
 import PlaygroundSupport
 
+public class ViewController: UIViewController {
+  let buttonView = ButtonView(frame: CGRect(x: 90, y: 450, width: 200, height: 40))
+  let linkedListCV = LinkedListCV(frame: CGRect(x: 0, y: 100, width: 376, height: 200))
+  
+  var linkList = LinkedList<Int>() {
+    didSet {
+      linkedListCV.collectionView.reloadData()
+    }
+  }
+  
+  public override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .white
+    view.addSubview(linkedListCV)
+    view.addSubview(buttonView)
+    linkedListCV.collectionView.dataSource = self
+    linkedListCV.collectionView.delegate = self
+    setUpButtonAction()
+  }
+  
+  public func setUpButtonAction(){
+    buttonView.appendButton.addTarget(self, action: #selector(appendButtonPressed), for: .touchUpInside)
+    buttonView.removeLastButton.addTarget(self, action: #selector(removeLastButtonPressed), for: .touchUpInside)
+  }
+  
+  @objc public func appendButtonPressed(){
+    linkList.append(Int.random(in: 0...100))
+    UIView.animate(withDuration: 0.9, animations: {
+      let indexPath = IndexPath(item: self.linkList.count - 1, section: 0)
+      self.linkedListCV.collectionView.insertItems(at: [indexPath])
+      self.linkedListCV.collectionView.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
+      guard let cell = self.linkedListCV.collectionView.cellForItem(at: indexPath) else {return}
+      cell.layer.borderColor = UIColor.orange.cgColor
+      cell.transform = CGAffineTransform(translationX: 8, y: -50)
+    })
+  }
+  @objc public func removeLastButtonPressed(){
+    
+    UIView.animate(withDuration: 0.9, animations: {
+      self.linkList.removeLast()
+      let indexPath = IndexPath(item: self.linkList.count , section: 0)
+      guard let cell = self.linkedListCV.collectionView.cellForItem(at: indexPath) else {return}
+      self.linkedListCV.collectionView.deleteItems(at: [indexPath])
+      cell.transform = CGAffineTransform(translationX: 40, y: 140)
+    }, completion: nil)
+  }
+}
+extension ViewController: UICollectionViewDataSource,UICollectionViewDelegateFlowLayout {
+  public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    
+    return linkList.count
+  }
+  public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    let cell = linkedListCV.collectionView.dequeueReusableCell(withReuseIdentifier: "NodeCell", for: indexPath) as! CustomCell
+    cell.layer.cornerRadius = cell.bounds.width / 2.0
+    cell.dataButton.setTitle("\(linkList[indexPath.row])", for: .normal)
+    return cell
+  }
+}
+
 let viewcontroller = ViewController()
 PlaygroundPage.current.liveView = viewcontroller
